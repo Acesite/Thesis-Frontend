1dabaf94c81ce7a2cc7f1c092ac51443a7b42d92
Acesite <meneznicole117477@gmail.com>
2025-12-01 21:45:03 +0800

    okira

--- src/components/AdminCrop/AdminMapBox.js
+++ src/components/AdminCrop/AdminMapBox.js
@@ -2090,7 +2090,7 @@ const AdminMapBox = () => {
             {/* past season */}
             <div className="rounded-lg border border-gray-200 bg-gray-50 px-3 py-2">
               <p className="text-[11px] font-semibold text-gray-800">
-                Past season
+                Previous season
               </p>
 
               {hasPastSeason ? (
